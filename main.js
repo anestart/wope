5cfd726778a7d18f420a436260ca173a5e9fcad6
Andres Perez <andresm.busqueda@gmail.com>
2023-06-25 18:48:49 -0400

    Interaccion carrusel

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 
 const header = document.querySelector('header');
+const carousel = document.querySelector('.carousel');
+const carouselItems = Array.from(carousel.children);
+const leftArrow = document.querySelector('#left');
+const rightArrow = document.querySelector('#right');
+const itemWidth = carouselItems[0].getBoundingClientRect().width;
+
+leftArrow.addEventListener('click', () => {
+  carousel.scrollBy(-itemWidth, 0);
+});
+
+rightArrow.addEventListener('click', () => {
+  carousel.scrollBy(itemWidth, 0);
+});
+
+
+
+
+
+
+
 
 window.addEventListener('scroll', function() {
     header.classList.toggle("sticky", window.scrollY > 0);
